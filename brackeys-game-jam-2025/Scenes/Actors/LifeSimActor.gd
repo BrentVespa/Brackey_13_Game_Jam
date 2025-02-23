f28efbbc65f5ee93a1b34e8ebadca35a2515fcc0
Group05Games <group05games@gmail.com>
2025-02-23 13:05:16 -0500

    Update LifeSimActor.gd

--- brackeys-game-jam-2025/Scenes/Actors/LifeSimActor.gd
+++ brackeys-game-jam-2025/Scenes/Actors/LifeSimActor.gd
@@ -15,15 +15,15 @@ func takeDamage(damage: int = 1):
 		self.die()
 
 # Heal damage. If no value for heal is passed, 1 health is gained.
-func heal(heal: int = 1):
+func heal(heal: int = 1) -> void:
 	self.health += heal
 	if (self.getCurrentHealth() > maxHealth):
 		self.health = maxHealth
 
 # Get health
-func getCurrentHealth():
+func getCurrentHealth() -> int:
 	return self.health
 
 # Kill this actor
-func die():
+func die() -> void:
 	self.queue_free()
